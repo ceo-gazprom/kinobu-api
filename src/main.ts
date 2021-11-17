e3594d4a008de1f8c0d73c6ef1f7d5e60e08420e
wacker <wackeryi@gmail.com>
2021-11-17 19:46:57 +0300

    ADD helmet protection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as helmet from 'helmet';
 import { initSwagger } from './common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  /**
+   * Protection against vulnerabilities by setting HTTP headers.
+   */
+  app.use(helmet());
 
   /**
    * Turn on the display of documentation if we are in the development environment
