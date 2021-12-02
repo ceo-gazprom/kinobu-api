751522bb607f6a7d0d28980881e620625ba3bcd8
wacker <wackeryi@gmail.com>
2021-12-03 06:37:44 +0700

    ADD logger options & change env value in condition

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,36 @@ import * as helmet from 'helmet';
 import * as RateLimit from 'express-rate-limit';
 import * as compression from 'compression';
 import * as bodyParser from 'body-parser';
-import { setupSwagger } from './common';
+import { setupLogLevels, setupSwagger } from './common';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<NestExpressApplication> {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: setupLogLevels(process.env.NODE_ENV === 'production'),
+  });
 
   /**
    * Protection against vulnerabilities by setting HTTP headers.
+   * @see https://docs.nestjs.com/security/helmet
    */
   app.use(helmet());
 
+  /**
+   * To enable CORS
+   * @see https://docs.nestjs.com/security/cors
+   */
   app.enableCors();
 
+  /**
+   * Setting a prefix for each route
+   * @see https://docs.nestjs.com/faq/global-prefix
+   */
   app.setGlobalPrefix('api');
 
+  /**
+   * Automatic validation at the application level, endpoints are protected from receiving incorrect data.
+   * @see https://docs.nestjs.com/techniques/validation
+   */
   app.useGlobalPipes(new ValidationPipe());
 
   app.use(compression());
@@ -37,7 +52,7 @@ async function bootstrap(): Promise<NestExpressApplication> {
   /**
    * Turn on the display of documentation if we are in the development environment
    */
-  if (process.env.ENVIRONMENT == 'dev') setupSwagger(app);
+  if (process.env.ENVIRONMENT == 'development') setupSwagger(app);
 
   await app.listen(Number(process.env.API_PORT));
 
